9af79c8cb0a852246195d731f21d458bc34d01ef
Alessandro Tatoli <vitoalessandro98@hotmail.com>
2021-05-17 17:28:32 -0400

    validaciones carreras opcionales, idiomas, servicio y electivas

--- modelo/views.py
+++ modelo/views.py
@@ -2,9 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import json
 import numpy as np
-
-
-# from keras.models import load_model
+from keras.models import load_model
 
 
 def index(request):
@@ -45,6 +43,69 @@ def render_flujograma(request, ci):
                         break
             fileN.close()
 
+        carrera = historico["Plan de Estudios"]
+
+        # materias BPELI (idiomas modernos)
+        mat_IM = {
+            'BPELI41': [
+                0, 'ALEMAN I'
+            ],
+            'BPELI31': [
+                0, 'FRANCES I'
+            ],
+            'BPELI42': [
+                0, 'ALEMAN II'
+            ],
+            'BPELI32': [
+                0, 'FRANCES II'
+            ],
+            'BPELI43': [
+                0, 'ALEMÁN III'
+            ],
+            'BPELI33': [
+                0, 'FRANCES III'
+            ],
+            'BPELI44': [
+                0, 'ALEMAN IV'
+            ],
+            'BPELI34': [
+                0, 'FRANCES IV'
+            ],
+            'BPELI45': [
+                0, 'ALEMAN V'
+            ],
+            'BPELI35': [
+                0, 'FRANCES V'
+            ],
+            'BPELI55': [
+                0, 'ITALIANO V'
+            ],
+            'BPELI46': [
+                0, 'ALEMAN VI'
+            ],
+            'BPELI36': [
+                0, 'FRANCES VI'
+            ],
+            'BPELI56': [
+                0, 'ITALIANO VI'
+            ],
+            'BPELI47': [
+                0, 'ALEMAN VII'
+            ],
+            'BPELI37': [
+                0, 'FRANCES VII'
+            ],
+            'BPELI48': [
+                0, 'ALEMAN VIII'
+            ],
+            'BPELI38': [
+                0, 'FRANCES VIII'
+            ],
+            'BPELI58': [
+                0, 'ITALIANO VIII'
+            ],
+        }
+
         # verificar materias que cursó el estudiante
         for materiaF in flujograma.keys():
             for trimestre in historico["Historico"]:
@@ -64,12 +125,39 @@ def render_flujograma(request, ci):
                             flujograma[materiaF][0] = 1
                             salto = True
                             break
+
+                    # validacion para servicio comunitario
+                    if materiaH != 1903 and materiaF == 'BPTDI01' and 'BPTDI01-1' in materiaH:
+                        if materiaH.split('_')[1] == 'Good':
+                            flujograma[materiaF][0] = 1
+                            trimestre[n] = 1903
+                            break
+
+                    if materiaH != 1903 and materiaF == 'BPTDI01' and 'BPTDI01-2' in materiaH:
+                        if materiaH.split('_')[1] == 'Good':
+                            trimestre[n] = 1903
+                            flujograma[materiaF][0] = 2
+                            salto = True
+                            break
+
+                    # validacion idiomas modernos
+                    if carrera == 'IDIOMAS MODERNOS' and materiaH != 1903 and 'BPELI' in materiaF and 'BPELI' in materiaH:
+                        if materiaH.split('_')[1] == 'Good' or materiaH.split('_')[1] == 'Excellent':
+                            trimestre[n] = 1903
+                            flujograma[materiaF][0] = 1
+                            mat_IM[materiaH.split('_')[0]][0] = 1
+                            flujograma[materiaF].append(mat_IM[materiaH.split('_')[0]][1])
+                            salto = True
+                            break
+
                 if salto:
                     break
 
     fileF.close()
 
-    carrera = historico["Plan de Estudios"]
+    if carrera == 'IDIOMAS MODERNOS':
+        return render(request, "flujograma.html",
+                      {'flujograma': flujograma, 'carrera': carrera, 'ci': ci, 'materias_op': mat_IM})
 
     return render(request, "flujograma.html", {'flujograma': flujograma, 'carrera': carrera, 'ci': ci})
 
